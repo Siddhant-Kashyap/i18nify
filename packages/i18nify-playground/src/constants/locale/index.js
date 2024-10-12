c6d27a97accb9936571067e41f07000b14a3b176
Siddhant-Kashyap <kashyapsiddhant01@gmail.com>
2024-10-13 03:56:48 +0530

    adds the list in LANGS

--- packages/i18nify-playground/src/constants/locale/index.js
+++ packages/i18nify-playground/src/constants/locale/index.js
@@ -29,4 +29,279 @@ export const LANGS = [
     label: 'MY',
     icon: '/assets/icons/ic_flag_my.svg',
   },
+  {
+    value: 'ae-AE',
+    label: 'United Arab Emirates',
+    icon: '/assets/icons/ic_flag_ae.svg',
+  },
+  {
+    value: 'es-ES',
+    label: 'Spain',
+    icon: '/assets/icons/ic_flag_es.svg',
+  },
+  {
+    value: 'it-IT',
+    label: 'Italy',
+    icon: '/assets/icons/ic_flag_it.svg',
+  },
+  {
+    value: 'jp-JP',
+    label: 'Japan',
+    icon: '/assets/icons/ic_flag_jp.svg',
+  },
+  {
+    value: 'ru-RU',
+    label: 'Russia',
+    icon: '/assets/icons/ic_flag_ru.svg',
+  },
+  {
+    value: 'br-BR',
+    label: 'Brazil',
+    icon: '/assets/icons/ic_flag_br.svg',
+  },
+  {
+    value: 'cn-CN',
+    label: 'China',
+    icon: '/assets/icons/ic_flag_cn.svg',
+  },
+  {
+    value: 'ca-CA',
+    label: 'Canada',
+    icon: '/assets/icons/ic_flag_ca.svg',
+  },
+  {
+    value: 'mx-MX',
+    label: 'Mexico',
+    icon: '/assets/icons/ic_flag_mx.svg',
+  },
+  {
+    value: 'kr-KR',
+    label: 'South Korea',
+    icon: '/assets/icons/ic_flag_kr.svg',
+  },
+  {
+    value: 'za-ZA',
+    label: 'South Africa',
+    icon: '/assets/icons/ic_flag_za.svg',
+  },
+  {
+    value: 'au-AU',
+    label: 'Australia',
+    icon: '/assets/icons/ic_flag_au.svg',
+  },
+  {
+    value: 'se-SE',
+    label: 'Sweden',
+    icon: '/assets/icons/ic_flag_se.svg',
+  },
+  {
+    value: 'no-NO',
+    label: 'Norway',
+    icon: '/assets/icons/ic_flag_no.svg',
+  },
+  {
+    value: 'dk-DK',
+    label: 'Denmark',
+    icon: '/assets/icons/ic_flag_dk.svg',
+  },
+  {
+    value: 'fi-FI',
+    label: 'Finland',
+    icon: '/assets/icons/ic_flag_fi.svg',
+  },
+  {
+    value: 'ch-CH',
+    label: 'Switzerland',
+    icon: '/assets/icons/ic_flag_ch.svg',
+  },
+  {
+    value: 'at-AT',
+    label: 'Austria',
+    icon: '/assets/icons/ic_flag_at.svg',
+  },
+  {
+    value: 'nl-NL',
+    label: 'Netherlands',
+    icon: '/assets/icons/ic_flag_nl.svg',
+  },
+  {
+    value: 'ar-AR',
+    label: 'Argentina',
+    icon: '/assets/icons/ic_flag_ar.svg',
+  },
+  {
+    value: 'cl-CL',
+    label: 'Chile',
+    icon: '/assets/icons/ic_flag_cl.svg',
+  },
+  {
+    value: 'co-CO',
+    label: 'Colombia',
+    icon: '/assets/icons/ic_flag_co.svg',
+  },
+  {
+    value: 'sa-SA',
+    label: 'Saudi Arabia',
+    icon: '/assets/icons/ic_flag_sa.svg',
+  },
+  {
+    value: 'sg-SG',
+    label: 'Singapore',
+    icon: '/assets/icons/ic_flag_sg.svg',
+  },
+  {
+    value: 'th-TH',
+    label: 'Thailand',
+    icon: '/assets/icons/ic_flag_th.svg',
+  },
+  {
+    value: 'tr-TR',
+    label: 'Turkey',
+    icon: '/assets/icons/ic_flag_tr.svg',
+  },
+  {
+    value: 'nz-NZ',
+    label: 'New Zealand',
+    icon: '/assets/icons/ic_flag_nz.svg',
+  },
+  {
+    value: 've-VE',
+    label: 'Venezuela',
+    icon: '/assets/icons/ic_flag_ve.svg',
+  },
+  {
+    value: 'vn-VN',
+    label: 'Vietnam',
+    icon: '/assets/icons/ic_flag_vn.svg',
+  },
+  {
+    value: 'eg-EG',
+    label: 'Egypt',
+    icon: '/assets/icons/ic_flag_eg.svg',
+  },
+  {
+    value: 'gr-GR',
+    label: 'Greece',
+    icon: '/assets/icons/ic_flag_gr.svg',
+  },
+  {
+    value: 'il-IL',
+    label: 'Israel',
+    icon: '/assets/icons/ic_flag_il.svg',
+  },
+  {
+    value: 'ir-IR',
+    label: 'Iran',
+    icon: '/assets/icons/ic_flag_ir.svg',
+  },
+  {
+    value: 'pk-PK',
+    label: 'Pakistan',
+    icon: '/assets/icons/ic_flag_pk.svg',
+  },
+  {
+    value: 'bd-BD',
+    label: 'Bangladesh',
+    icon: '/assets/icons/ic_flag_bd.svg',
+  },
+  {
+    value: 'ng-NG',
+    label: 'Nigeria',
+    icon: '/assets/icons/ic_flag_ng.svg',
+  },
+  {
+    value: 'pl-PL',
+    label: 'Poland',
+    icon: '/assets/icons/ic_flag_pl.svg',
+  },
+  {
+    value: 'pt-PT',
+    label: 'Portugal',
+    icon: '/assets/icons/ic_flag_pt.svg',
+  },
+  {
+    value: 'ro-RO',
+    label: 'Romania',
+    icon: '/assets/icons/ic_flag_ro.svg',
+  },
+  {
+    value: 'sk-SK',
+    label: 'Slovakia',
+    icon: '/assets/icons/ic_flag_sk.svg',
+  },
+  {
+    value: 'cz-CZ',
+    label: 'Czech Republic',
+    icon: '/assets/icons/ic_flag_cz.svg',
+  },
+  {
+    value: 'hu-HU',
+    label: 'Hungary',
+    icon: '/assets/icons/ic_flag_hu.svg',
+  },
+  {
+    value: 'id-ID',
+    label: 'Indonesia',
+    icon: '/assets/icons/ic_flag_id.svg',
+  },
+  {
+    value: 'ie-IE',
+    label: 'Ireland',
+    icon: '/assets/icons/ic_flag_ie.svg',
+  },
+  {
+    value: 'jm-JM',
+    label: 'Jamaica',
+    icon: '/assets/icons/ic_flag_jm.svg',
+  },
+  {
+    value: 'jo-JO',
+    label: 'Jordan',
+    icon: '/assets/icons/ic_flag_jo.svg',
+  },
+  {
+    value: 'ke-KE',
+    label: 'Kenya',
+    icon: '/assets/icons/ic_flag_ke.svg',
+  },
+  {
+    value: 'kg-KG',
+    label: 'Kyrgyzstan',
+    icon: '/assets/icons/ic_flag_kg.svg',
+  },
+  {
+    value: 'kh-KH',
+    label: 'Cambodia',
+    icon: '/assets/icons/ic_flag_kh.svg',
+  },
+  {
+    value: 'mo-MO',
+    label: 'Macau',
+    icon: '/assets/icons/ic_flag_mo.svg',
+  },
+  {
+    value: 'mt-MT',
+    label: 'Malta',
+    icon: '/assets/icons/ic_flag_mt.svg',
+  },
+  {
+    value: 'ph-PH',
+    label: 'Philippines',
+    icon: '/assets/icons/ic_flag_ph.svg',
+  },
+  {
+    value: 'ua-UA',
+    label: 'Ukraine',
+    icon: '/assets/icons/ic_flag_ua.svg',
+  },
+  {
+    value: 'tz-TZ',
+    label: 'Tanzania',
+    icon: '/assets/icons/ic_flag_tz.svg',
+  },
+  {
+    value: 'zw-ZW',
+    label: 'Zimbabwe',
+    icon: '/assets/icons/ic_flag_zw.svg',
+  },
 ];
